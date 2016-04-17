cf664b3bd166372e420ccbef8a380aece5353503
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2016-04-17 23:53:22 +0530

    Test Commit 4

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
+$recipient = "subhrajyoti12@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
